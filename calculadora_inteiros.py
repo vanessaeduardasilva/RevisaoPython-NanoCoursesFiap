dbc5994df6ca0105e76407c8362ec3d774cddd97
Vanessa Eduarda <vanessaeduarda279@gmail.com>
2026-03-23 20:44:14 -0300

    Primeiro commit com exercícios de Python

---
+++ calculadora_inteiros.py
@@ -0,0 +1,13 @@
+print("Boas Vindas ao Calculador de Inteiros!")
+prim_valor =int(input("Por favor, informe o primeiro valor: "))
+seg_valor =int(input("Por favor, informe o segundo valor: "))
+
+soma=(prim_valor) + (seg_valor)
+subrtracao = (prim_valor) - (seg_valor)
+divisao = (prim_valor) / (seg_valor)
+multiplicacao = (prim_valor) * (seg_valor)
+
+print(f"O resultado da soma foi: {soma}")
+print(f"O resultado da subtração foi: {subrtracao}")
+print(f"O resultado da divisão foi: {divisao}")
+print(f"O resultado da multiplicação foi: {multiplicacao}")
---
+++ velocidade_media.py
@@ -0,0 +1,7 @@
+print("Simulador de computador de bordo!")
+distancia = float(input("Por favor, informe a distância percorrida : "))
+tempo = float(input("Por favor, informe o tempo da viagem em horas: "))
+
+velocidade_media = distancia / tempo
+
+print(f"A velocidade média da viagem foi de: {velocidade_media} km/h")
