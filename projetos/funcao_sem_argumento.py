311ff7ad95c0679cf539f772748f5e2ba0ae90b1
Vanessa Eduarda <vanessaeduarda279@gmail.com>
2026-04-13 22:11:46 -0300

    função sem argumento

---
+++ projetos/funcao_sem_argumento.py
@@ -0,0 +1,10 @@
+def calcular_velocidade_media():
+    #codigo da nossa função
+    velocidade_media = distancia / tempo
+    #exibir resultado
+    print(f"A média é {velocidade_media}")
+
+#solicitar distância e tempo
+distancia = float(input("Digite a distância percorrida: "))
+tempo  = float(input("Digite o tempo da viagem: "))
+calcular_velocidade_media()
